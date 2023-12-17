a583383271e13abafd81a59b818893098fab767e
hzqd <hzqelf@yeah.net>
2023-12-18 01:37:17 +0800

    channel

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,40 @@
-use futures::{future::join_all, TryFutureExt};
-use std::{env::args, fs, time::Instant, vec};
-use tokio::net::TcpStream;
+use futures::future::join_all;
+use std::{env::args, fs, time::Instant, ops::Add};
+use tokio::{net::TcpStream, sync::watch::{Receiver, channel}};
 
-async fn tcp_scan() -> Vec<u8> {
+async fn tcp_scan(rx: Receiver<Option<String>>) -> Vec<u8> {
     let mut handles = vec![];
-    let addr = &args().into_iter().collect::<Vec<_>>()[1];
     for i in 1..=65535 {
-        let handle = TcpStream::connect(format!("{addr}:{i}")).map_ok_or_else(
-            move |_| format!("{addr}:{i} 关闭了\n"),
-            move |_| format!("{addr}:{i} 打开了！！！\n"),
-        );
+        let mut rx = rx.clone();
+        let handle = async move {
+            rx.changed().await.unwrap();
+            let addr = format!("{}:{i}", *rx.borrow().as_ref().unwrap());
+            match TcpStream::connect(&addr).await {
+                Ok(_) => addr.add(" 打开了\n").as_bytes().to_vec(),
+                Err(_) => addr.add(" 关闭了\n").as_bytes().to_vec(),
+            }
+        };
         handles.push(handle);
     }
     join_all(handles)
         .await
         .into_iter()
-        .flat_map(|s| s.as_bytes().to_vec())
-        .collect()
+        .flatten()
+        .collect::<Vec<_>>()
+}
+
+async fn exe_tcp_scan() -> Vec<u8> {
+    let (tx, rx) = channel(None);
+    let res = tcp_scan(rx);
+    let addr = args().into_iter().nth(1);
+    tx.send(addr).unwrap();
+    res.await
 }
 
 #[tokio::main]
 async fn main() {
     let start = Instant::now();
-    fs::write("result.txt", tcp_scan().await).unwrap();
+    fs::write("result.txt", exe_tcp_scan().await).unwrap();
     let elapsed = start.elapsed().as_secs();
     println!("Execution Time: {} Secs.", elapsed);
 }
